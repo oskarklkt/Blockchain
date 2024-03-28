67d00cf8851b089672f34f265e8f8cec8eb62e43
oklekot <oklekot@griddynamics.com>
2024-03-28 18:48:06 +0100

    stage 4 fixed

--- src/main/java/com/griddynamics/blockchain/validators/BlockchainValidator.java
+++ src/main/java/com/griddynamics/blockchain/validators/BlockchainValidator.java
@@ -3,22 +3,20 @@ package com.griddynamics.blockchain.validators;
 import com.griddynamics.blockchain.block.Block;
 import com.griddynamics.blockchain.blockchain.Blockchain;
 import com.griddynamics.blockchain.constant.AppConstants;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class BlockchainValidator {
   private final Blockchain blockchain;
-
-  public BlockchainValidator(Blockchain blockchain) {
-    this.blockchain = blockchain;
-  }
 
   public synchronized boolean validateNewBlock(Block block) {
     String previousBlockHash =
         blockchain.getBlocks().isEmpty()
             ? AppConstants.ZERO
             : blockchain.getBlocks().get(blockchain.getBlocks().size() - 1).getHash();
-      if (!block.getHash().startsWith(AppConstants.ZERO.repeat(blockchain.requiredTrailingZeros))) {
-        return false;
-      }
+    if (!block.getHash().startsWith(AppConstants.ZERO.repeat(blockchain.requiredTrailingZeros))) {
+      return false;
+    }
     return previousBlockHash.equals(block.getPreviousBlockHash());
   }
 }
